8216add38b0833639137bf46bccb8417a82d606b
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2018-10-10 08:36:53 -0600

    update js with users guess state and added class in cs for the disabled clear button state

--- scripts.js
+++ scripts.js
@@ -2,15 +2,16 @@
 
 // ZERO STATE
 // Set initial custom range from range text and generate first random number in that range.
-var minRangeInput = parseInt(document.querySelector('.min-range-text').innerText);
-var maxRangeInput = parseInt(document.querySelector('.max-range-text').innerText);
+var minRangeInputInitial = parseInt(document.querySelector('.min-range-text').innerText);
+var maxRangeInputInitial = parseInt(document.querySelector('.max-range-text').innerText);
 
-var randomNumber = assignRandomNumber(minRangeInput, maxRangeInput);
+
+var randomNumber = assignRandomNumber(minRangeInputInitial, maxRangeInputInitial);
 console.log('The initial random number is: ' + randomNumber);
 
 function assignRandomNumber(min, max) {
   var num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num
+  return num;
 };
 
 // Get custom range inputs from user and update min/max range and text when update button is clicked.
@@ -28,9 +29,11 @@ function getNewMinMax() {
   document.querySelector('.max-range-text').innerText = maxRangeInputNew;
 };
 
-// Get user's new guess when submit guess button is clicked
+// Get user's new guess when submit guess button is clicked,  displays that guess and the result
 var submitButton = document.querySelector('.submit-button');
 var guessInputBox = document.querySelector('.lg-input-box');
+var displayRecentGuess = document.querySelector('.guess-number');
+var displayResult = document.querySelector('.guess-result');
 
 submitButton.addEventListener('click', getUserGuess);
 
@@ -38,6 +41,15 @@ function getUserGuess() {
   var usersGuess = guessInputBox.value;
   console.log('The users guess is: ' + usersGuess);
   clearGuessInput();
+  displayRecentGuess.innerText = usersGuess;
+
+    if (usersGuess > randomNumber) {
+    displayResult.innerText = 'Sorry, that is too High';
+  } else if (usersGuess < randomNumber){
+    displayResult.innerText = 'Sorry, that is too low';
+  } else {
+    displayResult.innerText = 'BOOM!'
+  }
 };
 
 // Interactive clear button to clear user guess input field
@@ -50,6 +62,18 @@ function clearGuessInput() {
 };
 
 // Interactive reset button to reset game - done in HTML
+
+// disabling clear button and adding styling change when no text in the guess input
+//putting the clear back to original state if there is something there isn't working now
+if (guessInputBox.innerText === '') {
+  clearButton.disabled = true;
+  clearButton.classList.add('disable-clear');
+} else {
+  clearButton.disabled = false;
+  clearButton.classList.remove('disable-clear');
+}
+
+
 
 
 
